d0e2f433b85e59fc1a877823f32dbeb7ca092ee7
Paul Moore <pdmoore@gmail.com>
2020-07-26 14:59:51 -0700

    force failing test to pass

--- ISBN.Tests/ISBN10Test.cs
+++ ISBN.Tests/ISBN10Test.cs
@@ -52,7 +52,7 @@ namespace ISBN {
         public void Failing_Test_To_Demo_CI_Automation() {
             // This test fails
             // Fix it and commit to trunk and observe the CI build starts and passes
-            Assert.True(false, "Remove this test or change 'false' to true'");
+            Assert.True(true, "Remove this test or change 'false' to true'");
         }
     }
 
