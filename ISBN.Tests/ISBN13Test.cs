3882f50885f0a193ca4960caf5fd3c03903dcd2e
russellmcclure <60902669+russellmcclure@users.noreply.github.com>
2020-07-31 14:20:50 -0700

    Added base 13 validation

---
+++ ISBN.Tests/ISBN13Test.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using BookInfoProvider;
+using Xunit;
+
+namespace ISBN
+{
+
+   public class ISBN13Test
+   {
+      const string invalidIsbnErrorMessage = "ISBN is invalid";
+
+      [Fact]
+      public void ISBN_ShorterThan13Characters_ReturnsInvalidBookInfo()
+      {
+         //Arrange
+         string shortISBN = "12345";
+
+         //Act
+         ISBNFinder sut = new ISBNFinder();
+         BookInfo actual = sut.lookup13(shortISBN);
+
+         //Assert
+         Assert.Equal(invalidIsbnErrorMessage, actual.Title);
+      }
+
+      [Fact]
+      public void ISBN_LongerThan13Characters_ReturnsInvalidBookInfo()
+      {
+         string longISBN = "123456789ABCEDF";
+
+         ISBNFinder sut = new ISBNFinder();
+         BookInfo actual = sut.lookup13(longISBN);
+
+         Assert.Equal(invalidIsbnErrorMessage, actual.Title);
+      }
+
+      [Fact]
+      public void ISBN_BookAvailableFromFinder()
+      {
+         String unknownISBN = "978-0545790352";
+
+         ISBNFinder sut = new ISBNFinder();
+         BookInfo actual = sut.lookup13(unknownISBN);
+
+         Assert.Equal("Title not found", actual.Title);
+      }
+
+      [Fact]
+      public void ISBN_BookFound()
+      {
+         string ISBN = "9780321146533";
+
+         ISBNFinder sut = new ISBNFinder();
+         BookInfo actual = sut.lookup13(ISBN);
+
+         BookInfo expected = new BookInfo("Test Driven Development by Example", "Kent Beck", "0321146530", "9780321146533");
+         Assert.Equal(expected.ToString(), actual.ToString());
+      }
+
+      [Fact]
+      public void Failing_Test_To_Demo_CI_Automation()
+      {
+         // This test fails
+         // Fix it and commit to trunk and observe the CI build starts and passes
+         //Assert.True(false, "Remove this test or change 'false' to true'");
+      }
+   }
+}
---
+++ ISBN13Validator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ISBN
+{
+   public class ISBN13Validator
+   {
+      public static bool Validate(string isbn)
+      {
+         isbn = RemoveValidWhitespace(isbn);
+
+         // make sure it's 13 digits
+         if (isbn.Length == 13)
+         {
+            foreach (char ch in isbn)
+            {
+               if (!char.IsDigit(ch))
+               {
+                  return false;
+               }
+            }
+
+            return ValidateCheckSumIsbn13(isbn);
+         }
+
+         return false;
+      }
+
+      private static string RemoveValidWhitespace(string isbn)
+      {
+         return isbn.Replace(" ", "").Replace("-", "");
+      }
+
+      private static bool ValidateCheckSumIsbn13(string number)
+      {
+         int checkSumTotal = 0;
+
+         for (int i = 0; i < number.Length - 1; i++)
+         {
+            var numberDigit = number[i] - '0';
+            int multiplyBy = (((i % 2) == 1) ? 3 : 1);
+            //Console.WriteLine("RUSS" + numberDigit.ToString() + "-" + multiplyBy.ToString());
+            checkSumTotal += numberDigit * multiplyBy;
+         }
+
+         int calculatedCheckSum = 10 - (checkSumTotal % 10);
+         int givenCheckSum = number[12] - '0';
+         //Console.WriteLine("TheEnd:" + givenCheckSum.ToString() + "-" + calculatedCheckSum.ToString());
+
+         return calculatedCheckSum == givenCheckSum;
+      }
+   }
+}
