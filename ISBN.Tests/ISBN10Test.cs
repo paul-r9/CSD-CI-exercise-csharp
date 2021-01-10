46b10abb7a9327d1bf6ceeff1022563d82cdf287
Paul Moore <pdmoore@gmail.com>
2021-01-10 11:00:28 -0800

    revert to starting point

--- ISBN.Tests/ISBN10Test.cs
+++ ISBN.Tests/ISBN10Test.cs
@@ -2,87 +2,62 @@ using System;
 using BookInfoProvider;
 using Xunit;
 
-namespace ISBN {
-    public class ISBN10Test {
+namespace ISBN
+{
+    public class ISBN10Test
+    {
         [Fact]
-        public void ISBN_ShorterThan10Characters_ReturnsInvalidBookInfo() {
+        public void ISBN_ShorterThan10Characters_ReturnsInvalidBookInfo()
+        {
             //Arrange
             string shortISBN = "12345";
 
             //Act
             ISBNFinder sut = new ISBNFinder();
             BookInfo actual = sut.lookup(shortISBN);
-            
+
             //Assert
             Assert.Equal("ISBN must be 10 characters in length", actual.Title);
         }
 
         [Fact]
-        public void ISBN_LongerThan10Characters_ReturnsInvalidBookInfo() {
+        public void ISBN_LongerThan10Characters_ReturnsInvalidBookInfo()
+        {
             string longISBN = "123456789ABCEDF";
 
             ISBNFinder sut = new ISBNFinder();
             BookInfo actual = sut.lookup(longISBN);
-            
+
             Assert.Equal("ISBN must be 10 characters in length", actual.Title);
         }
 
         [Fact]
-        public void ISBN_BookAvailableFromFinder() {
+        public void ISBN_BookAvailableFromFinder()
+        {
             String unknownISBN = "0553562614";
-            
+
             ISBNFinder sut = new ISBNFinder();
             BookInfo actual = sut.lookup(unknownISBN);
-            
+
             Assert.Equal("Title not found", actual.Title);
         }
 
         [Fact]
-        public void ISBN_BookFound() {
+        public void ISBN_BookFound()
+        {
             string ISBN = "0321146530";
 
             ISBNFinder sut = new ISBNFinder();
             BookInfo actual = sut.lookup(ISBN);
 
-            BookInfo expected = new BookInfo("Test Driven Development by Example", "Kent Beck", "0321146530", "9780321146533");
+            BookInfo expected = new BookInfo("Test Driven Development by Example", "Kent Beck", "0321146530",
+                "9780321146533");
             Assert.Equal(expected.ToString(), actual.ToString());
         }
 
-        [Fact]
-        public void ISBN_X_Out_OF_Place()
-        {
-            String unknownISBN = "12345678X9";
-
-            ISBNFinder sut = new ISBNFinder();
-            BookInfo actual = sut.lookup(unknownISBN);
-
-            Assert.Equal("ISBN has an X out of place", actual.Title);
-        }
-
-        [Fact]
-        public void ISBN_Invalid_Characters()
-        {
-            String unknownISBN = "ABCDEFGHIJK";
-
-            ISBNFinder sut = new ISBNFinder();
-            BookInfo actual = sut.lookup(unknownISBN);
-
-            Assert.Equal("ISBN using invalid characters", actual.Title);
-        }
-
-        [Fact]
-        public void ISBN_WrongChecksum()
-        {
-            string wrongISBN = "0478084523";
-
-            ISBNFinder sut = new ISBNFinder();
-            BookInfo actual = sut.lookup(wrongISBN);
-
-            Assert.Equal("ISBN has wrong checksum", actual.Title);
-        }
-
         [Fact(Skip = "skipping this test should get a Green azure build")]
-        public void Failing_Test_To_Demo_CI_Automation() {
+        public void Failing_Test_To_Demo_CI_Automation()
+        {
             // This test fails
             // Fix it and commit to trunk and observe the CI build starts and passes
             Assert.True(false, "Remove this test or change 'false' to true'");
--- ISBNFinder.cs
+++ ISBNFinder.cs
@@ -1,6 +1,4 @@
 using BookInfoProvider;
-using System;
-using System.Text.RegularExpressions;
 
 namespace ISBN {
     public class ISBNFinder {
@@ -14,28 +12,9 @@ namespace ISBN {
         }
         
         public BookInfo lookup(string ISBN) {
-            ISBN = ISBN.Replace(" ", "").Replace("-", "");
-            Regex re = new Regex(@"[0-9X]");
-            if(!re.IsMatch(ISBN))
-            {
-                BookInfo badISBN = new BookInfo("ISBN using invalid characters");
-                return badISBN;
-            }
             
-            if(!(ISBN.IndexOf("X") == 9 || ISBN.IndexOf("X") == -1))
-            {
-                BookInfo badISBN = new BookInfo("ISBN has an X out of place");
-                return badISBN;
-            }
-
             if (ISBN.Length != 10) {
                 BookInfo badISBN = new BookInfo("ISBN must be 10 characters in length");
-                return badISBN;
-            }
-
-            if (!validChecksum(ISBN))
-            {
-                BookInfo badISBN = new BookInfo("ISBN has wrong checksum");
                 return badISBN;
             }
 
@@ -47,25 +26,5 @@ namespace ISBN {
             
             return bookInfo;
         }
-
-        public bool validChecksum(string ISBN)
-        {
-            int accumulator = 0;
-            for(int i = 0; i < ISBN.Length - 1; i++)
-            {
-                int pos = int.Parse(ISBN[i].ToString());
-                accumulator += pos * (i + 1);
-            }
-            accumulator %= 11;
-            if(accumulator == 10)
-            {
-                return ISBN[ISBN.Length - 1] == 'X';
-            }
-            else
-            {
-                return ISBN[ISBN.Length - 1] == accumulator.ToString().ToCharArray()[0];
-            }
-        }
-
     }
 }
