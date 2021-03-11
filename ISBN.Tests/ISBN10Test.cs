4285b7def955bda3bd894d0787fe45a9eba08827
Paul Moore <pdmoore@gmail.com>
2021-03-11 09:19:43 -0800

    force failin test, expect Azure build to fail

--- ISBN.Tests/ISBN10Test.cs
+++ ISBN.Tests/ISBN10Test.cs
@@ -55,7 +55,8 @@ namespace ISBN
             Assert.Equal(expected.ToString(), actual.ToString());
         }
 
-        [Fact(Skip = "skipping this test should get a Green azure build")]
+        // [Fact(Skip = "skipping this test should get a Green azure build")]
+        [Fact]
         public void Failing_Test_To_Demo_CI_Automation()
         {
             // This test fails
