2158e594eda33d51304a6a571305ff2355bc2ee4
Nick Sanford <Nick.Sanford@cunamutual.com>
2021-03-18 15:30:49 -0500

    fixing issue with adding 'or 13' to bad isbn message in tests

--- ISBN.Tests/ISBN10Test.cs
+++ ISBN.Tests/ISBN10Test.cs
@@ -17,7 +17,7 @@ namespace ISBN
             BookInfo actual = sut.lookup(shortISBN);
 
             //Assert
-            Assert.Equal("ISBN must be 10 characters in length", actual.Title);
+            Assert.Equal("ISBN must be 10 or 13 characters in length", actual.Title);
         }
 
         [Fact]
@@ -28,7 +28,7 @@ namespace ISBN
             ISBNFinder sut = new ISBNFinder();
             BookInfo actual = sut.lookup(longISBN);
 
-            Assert.Equal("ISBN must be 10 characters in length", actual.Title);
+            Assert.Equal("ISBN must be 10 or 13 characters in length", actual.Title);
         }
 
         [Fact]
