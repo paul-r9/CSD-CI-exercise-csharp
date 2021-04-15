8be9eddc37c044dcccbdb984e4374750cb6f93b5
André Catarino <andre.catarino@miniclip.com>
2021-04-15 22:58:43 +0100

    Add tests for ISBN13 with spaces and hyphens

--- ISBN.Tests/ISBN13Test.cs
+++ ISBN.Tests/ISBN13Test.cs
@@ -17,5 +17,31 @@ namespace ISBN
                 "9780321146533");
             Assert.Equal(expected.ToString(), actual.ToString());
         }
+        
+        [Fact]
+        public void ISBN13_WorksWithDashes()
+        {
+            string ISBN13 = "978-0-321-14653-3";
+
+            ISBNFinder sut = new ISBNFinder();
+            BookInfo actual = sut.Lookup(ISBN13);
+
+            BookInfo expected = new BookInfo("Test Driven Development by Example", "Kent Beck", "0321146530",
+                "9780321146533");
+            Assert.Equal(expected.ToString(), actual.ToString());
+        }
+        
+        [Fact]
+        public void ISBN13_WorksWithSpaces()
+        {
+            string ISBN13 = "978 0 321 14653 3";
+
+            ISBNFinder sut = new ISBNFinder();
+            BookInfo actual = sut.Lookup(ISBN13);
+
+            BookInfo expected = new BookInfo("Test Driven Development by Example", "Kent Beck", "0321146530",
+                "9780321146533");
+            Assert.Equal(expected.ToString(), actual.ToString());
+        }
     }
 }
