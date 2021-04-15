abb1864b9ea062d29940074789ef6f2a8366c05e
André Catarino <andre.catarino@miniclip.com>
2021-04-15 22:36:50 +0100

    Failing build to test CD

---
+++ ISBN.Tests/ISBN13Test.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace ISBN
+{
+    public class ISBN13Test
+    {
+        [Fact]
+        //[Fact]
+        // [Fact(Skip="Enable this test to see the CI build fail")]
+        public void Failing_Test_To_Demo_CI_Automation()
+        {
+            // This test fails
+            // Fix it and commit to trunk and observe the CI build starts and passes
+            Assert.True(false, "Remove this test or change 'false' to true'");
+        }
+    }
+}
