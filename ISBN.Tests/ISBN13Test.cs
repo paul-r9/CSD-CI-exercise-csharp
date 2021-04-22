11845a4a31c7031c207a2fb2075e0a1a21c1efb8
Anderson, Mark <Mark.Anderson@cunamutual.com>
2021-04-22 15:13:57 -0500

    add ISBN13Test file

---
+++ ISBN.Tests/ISBN13Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ISBN.Tests
+{
+    public class ISBN13Test
+    {
+        // [Fact(Skip = "skipping this test should get a Green azure build")]
+        [Fact]
+        public void Failing_Test_To_Demo_CI_Automation()
+        {
+            // This test fails
+            // Fix it and commit to trunk and observe the CI build starts and passes
+            Assert.True(false, "Remove this test or change 'false' to true'");
+        }
+    }
+}
