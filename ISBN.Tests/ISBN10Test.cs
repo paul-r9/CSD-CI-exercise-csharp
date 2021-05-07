daafc9a71b6399479f94bc4d9e219ae8ecd57024
Paul Moore <pdmoore@gmail.com>
2021-05-07 12:19:37 -0700

    skip failing test, expect Azure green build

--- ISBN.Tests/ISBN10Test.cs
+++ ISBN.Tests/ISBN10Test.cs
@@ -55,8 +55,8 @@ namespace ISBN
             Assert.Equal(expected.ToString(), actual.ToString());
         }
 
-        // [Fact(Skip = "skipping this test should get a Green azure build")]
-        [Fact]
+        [Fact(Skip = "skipping this test should get a Green azure build")]
+        // [Fact]
         public void Failing_Test_To_Demo_CI_Automation()
         {
             // This test fails
