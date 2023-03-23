533ad37c2839c7bf058e1321dd2ebeacd83f0c5e
aswest <awest@limra.com>
2023-03-23 15:50:28 -0400

    Got first test passing

---
+++ ISBN.Tests/ISBN13Test.cs
@@ -0,0 +1,25 @@
+﻿using BookInfoProvider;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ISBN.Tests
+{
+    public class ISBN13Test
+    {
+        [Fact]
+        public void ISBN13_IgnoresHyphens_WhenValidISBN()
+        {
+            string ISBNwithoutSpaces = "9781942788331";
+            string ISBNwithSpaces = "978-1-94-278833-1";
+
+            ISBNFinder sut = new ISBNFinder();
+            BookInfo actualWithoutSpaces = sut.Lookup(ISBNwithoutSpaces);
+            BookInfo actualWithSpaces = sut.Lookup(ISBNwithSpaces);
+
+            Assert.Equal("Accelerate", actualWithoutSpaces.Title);
+            Assert.Equal(actualWithSpaces.Title, actualWithoutSpaces.Title);
+        }
+    }
+}
--- ISBNFinder.cs
+++ ISBNFinder.cs
@@ -12,25 +12,30 @@ namespace ISBN {
         }
         
         public BookInfo Lookup(string isbn) {
-            
+
             // remove hyphens & spaces
-            
+            var cleanedISBN = isbn.Replace("-", string.Empty);
+
             // guard clauses
             // if it's 10 digits
-                // call a new method 10digit stuff
-                // handle 10 digit checksum validation
+            // call a new method 10digit stuff
+            // handle 10 digit checksum validation
             // else 
             // handle 13 digit checksum validation
+            if (cleanedISBN.Length == 13)
+            {
+                return isbnService.Retrieve(cleanedISBN);
+            }
             // else it's not a 10 or digit number - null case
-            
+
             // checksum is valid, do the lookup and return
-            
-            if (isbn.Length != 10) {
+
+            if (cleanedISBN.Length != 10) {
                 BookInfo badIsbn = new BookInfo("ISBN must be 10 characters in length");
                 return badIsbn;
             }
 
-            BookInfo bookInfo = isbnService.Retrieve(isbn);
+            BookInfo bookInfo = isbnService.Retrieve(cleanedISBN);
             
             if (null == bookInfo) {
                 return new BookInfo("Title not found");
