7a50de52d4bab05d3ff580711b499f357d857548
Paul Moore <pdmoore@gmail.com>
2023-03-23 12:40:47 -0700

    pseudocode for approach

--- ISBNFinder.cs
+++ ISBNFinder.cs
@@ -13,6 +13,18 @@ namespace ISBN {
         
         public BookInfo Lookup(string isbn) {
             
+            // remove hyphens & spaces
+            
+            // guard clauses
+            // if it's 10 digits
+                // call a new method 10digit stuff
+                // handle 10 digit checksum validation
+            // else 
+            // handle 13 digit checksum validation
+            // else it's not a 10 or digit number - null case
+            
+            // checksum is valid, do the lookup and return
+            
             if (isbn.Length != 10) {
                 BookInfo badIsbn = new BookInfo("ISBN must be 10 characters in length");
                 return badIsbn;
